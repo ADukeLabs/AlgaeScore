60d45ebf733fb11b081a2f3f2734e240890373f4
ADukeLabs <d.alex.duke@gmail.com>
2015-02-24 16:59:00 +1300

    gitEncoder added to class

--- AlgaeScore/AlgaeScore/ImageReader.cs
+++ AlgaeScore/AlgaeScore/ImageReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,30 +16,34 @@ namespace AlgaeScore
 
         }
         
-        public void pixelImage()
+        public void createPixelImage()
         {
             var image = Image.FromFile("");
             using (var context = image.CreateUnsafeContext())
+            for (var i = 0; i < context.Width; i++)
             {
-                for (var i = 0; i < context.Width; i++)
+                for (var e = 0; e < context.Height; e++)
                 {
-                    for (var e = 0; e < context.Height; e++)
-                    {
-                        var pixel = context.GetRawPixel(i, e);
-                        var average = Convert.ToByte((pixel.Red + pixel.Green + pixel.Blue) / 3d);
-                        context.SetPixel(i, e, pixel.Alpha, average, average, average);
-                    }
+                    var pixel = context.GetRawPixel(i, e);
+                    var average = Convert.ToByte((pixel.Red + pixel.Green + pixel.Blue) / 3d);
+                    context.SetPixel(i, e, pixel.Alpha, average, average, average);
                 }
             }
+            
+        }
+
+
+        public void gifEncoder()
+        {
+            using (var image = Image.FromFile(""))
+            using (var gif = File.OpenWrite(""))
+            using (var encoder = new GifEncoder(gif))
+                for (var i = 0; i < 360; i += 10)
+                    using (var frame = image.Rotate(i, false))
+                    {
+                        encoder.AddFrame(frame);
+                    }
         }
         
-        
-
-
-
-        
-
-
-
     }
 }
