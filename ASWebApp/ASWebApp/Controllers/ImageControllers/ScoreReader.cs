be6d9de6d86694a3e69f442bc01d222021406e14
ADukeLabs <d.alex.duke@gmail.com>
2015-03-23 11:36:55 +1300

    Web App Files changed and updated

---
+++ ASWebApp/ASWebApp/Controllers/ImageControllers/ScoreReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using BumpKit;
+
+namespace ASWebApp.Controllers.ImageControllers
+{
+    public class ScoreReader
+    {
+        //Create int variables called 'whiteScore' and 'blackScore'
+        public int whiteScore = 0;
+        public int blackScore = 0;
+        //private ImageTesting test1;
+
+        public decimal scoreCalculator()
+        {
+            decimal white = whiteScore;
+            decimal black = blackScore;
+
+            //Get instance of new bitmap image.
+            //Image picture = image;
+            var picture = Bitmap.FromFile("new.bmp");
+            using (var context = picture.CreateUnsafeContext())
+            {
+                for (var w = 0; w < context.Width; w++)
+                {
+                    for (var h = 0; h < context.Height; h++)
+                    {
+                        BumpKit.UnsafeBitmapContext.Pixel pixel = context.GetRawPixel(w, h);
+                        if (pixel.EqualsColor(Color.White))
+                        {
+                            white++;
+                        }
+                        else if (pixel.EqualsColor(Color.Black))
+                        {
+                            black++;
+                        }
+                    }
+                }
+            }
+            //Formula for percentage
+            // (count / totalcount) * 100);
+            decimal score = Math.Floor((white / (white + black)) * 100);
+            return score;
+        }
+    }
+}
---
+++ WebAlgaeScore/Models/Photos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace WebAlgaeScore.Models
+{
+    public class Photos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+        public string Description { get; set; }
+    }
+}
